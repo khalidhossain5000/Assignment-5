9ea93bff1b61369f7f59b7ac4b1ac7905166bee2
khalidhossain <mdkhalidhossain5000@gmail.com>
2025-03-01 13:59:39 +0600

    all task completed alert added

--- js/script.js
+++ js/script.js
@@ -5,3 +5,7 @@ completedEvent('card-btn-3','card-3-title');
 completedEvent('card-btn-4','card-4-title');
 completedEvent('card-btn-5','card-5-title');
 completedEvent('card-btn-6','card-6-title');
+ //ALL TASK COMPLETED ALERT-->
+ document.getElementById('card-btn-6').addEventListener('click',function(){
+    alert("Congrats!!!You have completed all the current task")
+})
