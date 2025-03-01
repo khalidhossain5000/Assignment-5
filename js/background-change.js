5197c551f1d831d268ac34afe4a94eac4cd9007e
khalidhossain <mdkhalidhossain5000@gmail.com>
2025-03-01 16:01:53 +0600

    Background Changing When Clicking On The Button Is Added

---
+++ js/background-change.js
@@ -0,0 +1,11 @@
+const body=document.getElementById('bg');
+document.getElementById('background-change-btn')
+.addEventListener('click',function(){
+    const random1=Math.trunc(Math.random()*256)
+    const random2=Math.trunc(Math.random()*256)
+    const random3=Math.trunc(Math.random()*256)
+
+    const rgbColor=`rgb(${random1},${random2},${random3})`
+    body.style.backgroundColor=rgbColor;
+    
+})
